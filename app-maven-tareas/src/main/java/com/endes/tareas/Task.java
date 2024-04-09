1cec94b5edb19184ec1c08060a33c6dd5070614d
acangon650 <acangon@g.educaand.es>
2024-04-09 20:07:07 +0200

    Encapsulacion de datos

---
+++ app-maven-tareas/src/main/java/com/endes/tareas/Task.java
@@ -0,0 +1,28 @@
+package com.endes.tareas;
+
+public class Task {
+	
+	private String title;
+	private boolean isCompleted;
+	
+	public Task(String title) {
+		this.title = title;
+		this.isCompleted = false;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}	
+}
